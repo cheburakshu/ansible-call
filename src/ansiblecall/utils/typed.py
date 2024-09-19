e8eb5686e54c4f9aeb44882c1f1a67d1ba563caf
cheburakshu <rajagopalasu@vmware.com>
2024-09-19 15:32:13 -0600

    Add type support for ansible modules

--- src/ansiblecall/utils/typed.py
+++ src/ansiblecall/utils/typed.py
@@ -2,10 +2,17 @@ import ast
 import contextlib
 import dataclasses
 import json
+import logging
+import multiprocessing
+import pathlib
+import shutil
 
 import yaml
 
 import ansiblecall
+import ansiblecall.utils.ansibleproxy
+
+log = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass(kw_only=True)
@@ -36,22 +43,14 @@ class Field:
 
     def format_default(self):
         ret = None
-        if (
-            self.type is bool
-            and isinstance(self.default, str)
-            and self.default is not None
-        ):
+        if self.type is bool and isinstance(self.default, str) and self.default is not None:
             if self.default.lower() in ["yes", "true"]:
                 ret = True
             elif self.default.lower() in ["no", "false"]:
                 ret = False
         elif self.type is str and self.default is not None:
             ret = f"{self.default!r}"
-        elif (
-            self.type is dict
-            and isinstance(self.default, str)
-            and self.default is not None
-        ):
+        elif self.type is dict and isinstance(self.default, str) and self.default is not None:
             ret = json.loads(self.default)
         elif (self.type is float or self.type is int) and self.default is not None:
             ret = self.type(self.default)
@@ -59,25 +58,25 @@ class Field:
 
     def __repr__(self):
         default = f"= {self.format_default()}" if self.optional else ""
-        description = (
-            " ".join(self.description)
-            if isinstance(self.description, list)
-            else self.description
-        )
-        choices = ("Choices: " + ", ".join(self.choices)) if self.choices else ""
+        description = " ".join(self.description) if isinstance(self.description, list) else self.description
+        choices = (self.choices and "Choices: " + str(self.choices)) or ""
         return f'{self.name}: {self.type.__name__} {default}\n"""{description} {choices}"""'
 
 
 class TypeFactory:
-    def __init__(self, input_: list[Field], output: list[Field], module_name: str):
-        self.input = input_
-        self.output = output
+    def __init__(
+        self,
+        type_dir: str,
+        module_name: dict,
+    ):
+        self.type_dir = type_dir
+        self.module_name = module_name
 
         self.output_class_name = None
         self.output_class_body = None
         self.input_class_name = None
         self.input_class_body = None
-        self.module_name = module_name
+        self.module_file_name = None
 
     @staticmethod
     def convert_fields_to_lines(fields):
@@ -100,15 +99,19 @@ class TypeFactory:
         return cls.align(lines)
 
     def generate(self):
+        mods = ansiblecall.utils.ansibleproxy.load_ansible_mods()
+        schema = self.get_io_schema(mod=mods[self.module_name])
         self.input_class_name = self.module_name.split(".")[2].capitalize()
         self.output_class_name = f"{self.input_class_name}Out"
-        self.output_class_body = self.generate_class_body(fields=self.output)
-        self.input_class_body = self.generate_class_body(fields=self.input)
-        return self.render_template()
+        self.output_class_body = self.generate_class_body(fields=schema["output"])
+        self.input_class_body = self.generate_class_body(fields=schema["input"])
+        self.module_file_name = f"{self.module_name.replace('.', '_')}.py"
+        code = self.render_template()
+        with open(pathlib.Path(self.type_dir).joinpath(self.module_file_name), "w") as fp:
+            fp.write(code)
 
     def render_template(self):
-        return f"""
-import dataclasses
+        return f"""import dataclasses
 import ansiblecall
 import ansiblecall.utils.typed
 
@@ -116,121 +119,166 @@ import ansiblecall.utils.typed
 @dataclasses.dataclass(kw_only=True)
 class {self.output_class_name}(ansiblecall.utils.typed.OutputBase):
 {self.output_class_body}
-
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
 @dataclasses.dataclass(kw_only=True)
 class {self.input_class_name}:
 {self.input_class_body}
+    # Method to filter out unset values or values left at their default
+    def get_params(self) -> dict:
+        ret = {{}}
+        for f in dataclasses.fields(self):
+            value = getattr(self, f.name)
+            # Check if the value is different from the default (or explicitly set)
+            if value is not None and value != f.default:
+                ret[f.name] = value
+        return ret
+
     def run(self) -> {self.output_class_name}:
         return {self.output_class_name}(**self.raw())
 
     def raw(self) -> dict:
-        return ansiblecall.module({self.module_name!r}, **dataclasses.asdict(self))
+        return ansiblecall.module({self.module_name!r}, **self.get_params())
 """
 
+    @classmethod
+    def process(cls, queue):
+        """
+        Run type generation using multiprocessing lib
+        """
+        procs = []
+        for _ in range(multiprocessing.cpu_count()):
+            p = multiprocessing.Process(name=cls.__name__, target=cls.generate_parallel, args=(queue,))
+            p.start()
+            procs.append(p)
+        for p in procs:
+            p.join()
 
-def get_var_value(mod_str: str, var: str) -> str:
-    """
-    Return value of a variable in a python module
-    """
-    return next(
-        (
-            n.value.value
-            for n in ast.walk(ast.parse(mod_str))
-            if isinstance(n, ast.Assign)
-            and hasattr(n.targets[0], "id")
-            and n.targets[0].id == var
-        ),
-        None,
-    )
+    @staticmethod
+    def get_var_value(mod_str: str, var: str) -> str:
+        """
+        Return value of a variable in a python module
+        """
+        return next(
+            (
+                n.value.value
+                for n in ast.walk(ast.parse(mod_str))
+                if isinstance(n, ast.Assign) and hasattr(n.targets[0], "id") and n.targets[0].id == var
+            ),
+            None,
+        )
 
+    @staticmethod
+    def parse_yaml(doc: str) -> dict:
+        """
+        Parse doc yaml
+        """
+        ret = {}
+        with contextlib.suppress(yaml.YAMLError):
+            ret = yaml.safe_load(doc) or {}
+        return ret
 
-def parse_yaml(doc: str) -> dict:
-    """
-    Parse doc yaml
-    """
-    ret = {}
-    with contextlib.suppress(yaml.YAMLError):
-        ret = yaml.safe_load(doc) or {}
-    return ret
-
-
-def parse_fragment(fragments):
-    """
-    Parse a doc fragment into a field schema
-    """
-    ret = []
-    type_map = {
-        "dict": dict,
-        "int": int,
-        "path": str,
-        "str": str,
-        "any": str,
-        "sid": str,
-        "float": float,
-        "bool": bool,
-        "jsonarg": str,
-        "complex": dict,
-        "json": str,
-        "raw": str,
-        "list": list,
-        None: str,
-    }
-    for name, fragment in fragments.items():
-        if not isinstance(fragment, dict):
-            continue
-        type_ = type_map[fragment.get("type", "str")]
-        elements = fragment.get("elements", "")
-        optional = not (fragment.get("always") or fragment.get("required"))
-        default = fragment.get("default")
-        description = fragment.get("description", "")
-        choices = fragment.get("choices")
-        ret.append(
-            Field(
-                name=name,
-                optional=optional,
-                elements=elements,
-                type=type_,
-                default=default,
-                description=description,
-                choices=choices,
+    @staticmethod
+    def parse_fragment(fragments):
+        """
+        Parse a doc fragment into a field schema
+        """
+        ret = []
+        type_map = {
+            "dict": dict,
+            "int": int,
+            "path": str,
+            "str": str,
+            "any": str,
+            "sid": str,
+            "float": float,
+            "bool": bool,
+            "jsonarg": str,
+            "complex": dict,
+            "json": str,
+            "raw": str,
+            "list": list,
+            None: str,
+        }
+        for name, fragment in fragments.items():
+            if not isinstance(fragment, dict):
+                continue
+            type_ = type_map[fragment.get("type", "str")]
+            elements = fragment.get("elements", "")
+            optional = not (fragment.get("always") or fragment.get("required"))
+            default = fragment.get("default")
+            description = fragment.get("description", "")
+            choices = fragment.get("choices")
+            ret.append(
+                Field(
+                    name=name,
+                    optional=optional,
+                    elements=elements,
+                    type=type_,
+                    default=default,
+                    description=description,
+                    choices=choices,
+                )
             )
-        )
-    return ret
+        return ret
 
+    @classmethod
+    def get_io_schema(cls, mod: dict) -> dict[str, str]:
+        """
+        Get input and output docs for a module
+        """
+        ret, mod_str = {}, ""
+        with open(mod.abs) as fp:
+            mod_str = fp.read()
+        for doc_var, var in (("DOCUMENTATION", "input"), ("RETURN", "output")):
+            val = cls.get_var_value(mod_str=mod_str, var=doc_var)
+            parsed = {}
+            if val:
+                parsed = cls.parse_yaml(val)
+            fragments = parsed.get("options") if "options" in parsed else parsed
+            ret[var] = cls.parse_fragment(fragments=fragments)
+        return ret
 
-def get_io_schema(mod: dict) -> dict[str, str]:
-    """
-    Get input and output docs for a module
-    """
-    ret, mod_str = {}, ""
-    with open(mod.abs) as fp:
-        mod_str = fp.read()
-    for doc_var, var in (("DOCUMENTATION", "input"), ("RETURN", "output")):
-        val = get_var_value(mod_str=mod_str, var=doc_var)
-        parsed = {}
-        if val:
-            parsed = parse_yaml(val)
-        fragments = parsed.get("options") if "options" in parsed else parsed
-        ret[var] = parse_fragment(fragments=fragments)
-    return ret
+    @staticmethod
+    def init_dirs(clean=None):
+        init_file = pathlib.Path(__file__).parent.parent.joinpath("typed", "__init__.py")
+        init_dir = init_file.parent
+        if clean:
+            log.info("Removing dir %s.", init_dir)
+            shutil.rmtree(init_dir)
+        init_dir.mkdir(parents=True, exist_ok=True)
+        log.info("Created dir %s.", init_dir)
+        init_file.touch()
+        return init_dir
 
+    @staticmethod
+    def generate_parallel(queue):
+        while not queue.empty():
+            factory = queue.get()
+            factory.generate()
+            with contextlib.suppress(NotImplementedError):
+                queue_size = queue.qsize()
+                if queue_size and queue_size % 500 == 0:
+                    log.info("%s modules remaining.", queue_size)
 
-def install(modules=None):
-    """
-    Install typings for ansible modules
-    """
-
-    mods = ansiblecall.refresh_modules()
-    type_mods = modules and list(set(modules) & set(mods)) or list(mods)
-    for mod in type_mods:
-        type_mod = mods[mod]
-        schema = get_io_schema(mod=type_mod)
-        factory = TypeFactory(
-            input_=schema["input"], output=schema["output"], module_name=mod
-        )
-        factory.generate()
-
-
-if __name__ == "__main__":
-    install(modules=["community.general.archive"])
+    @classmethod
+    def run(cls, modules=None):
+        """
+        Install typings for ansible modules
+        """
+        queue = multiprocessing.Queue()
+        mods = ansiblecall.refresh_modules()
+        type_mods = (modules and list(set(modules) & set(mods))) or list(mods)
+        clean = not modules
+        log.info("Initializing dirs.")
+        type_dir = cls.init_dirs(clean=clean)
+        log.info("Generating types for %s module(s).", len(type_mods))
+        for module_name in type_mods:
+            factory = cls(
+                type_dir=type_dir,
+                module_name=module_name,
+            )
+            queue.put(factory, block=False)
+        cls.process(queue=queue)
+        log.info("Done!")
---
+++ tests/integration/ansibleproxy/test_type_factory.py
@@ -0,0 +1,32 @@
+from ansiblecall.utils import typed
+
+
+def test_type_factory():
+    """Ensure ansible module can be called using typings"""
+    typed.TypeFactory.run(
+        modules=[
+            "ansible.builtin.ping",
+            "community.general.archive",
+            "ansible.builtin.file",
+        ]
+    )
+    import ansiblecall.typed.ansible_builtin_file as file
+    import ansiblecall.typed.ansible_builtin_ping as ping
+    import ansiblecall.typed.community_general_archive as archive
+
+    ret = ping.Ping(data="hello").run()
+    assert ret.ping == "hello"
+    ret = ping.Ping(data="hello").raw()
+    assert ret == {"ping": "hello"}
+    p = ping.Ping()
+    p.data = "ping"
+    assert p.run().ping == "ping"
+
+    ret = file.File(path="/tmp/foo", state="absent").run()
+    ret = file.File(path="/tmp/foo", state="touch").run()
+    assert ret.changed is True
+    ret = file.File(path="/tmp/foo.gz", state="absent").run()
+    ret = archive.Archive(path="/tmp/foo").run()
+    assert ret.changed is True
+    ret = archive.Archive(path="/tmp/foo").run()
+    assert ret.changed is False
